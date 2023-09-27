946afdfc4e2953ff6af1eb4bee2e829e1e1809bb
Prince Boamah <regioths@gmail.com>
2023-09-27 08:25:21 +0000

    modified

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,14 @@ package { 'puppet-lint':
   ensure   => '2.1.0',
   provider => 'gem',
 }
+
+# Ensure that pip3 is installed
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+# Install Flask version 2.1.0 using pip3
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
